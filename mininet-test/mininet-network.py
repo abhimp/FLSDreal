27b7488fb2a2e64ab0daa59c0e9d5cff909c2b0f
Abhijit Mondal <abhijit.manpur@gmail.com>
2020-05-23 19:17:16 +0530

    adding trace file support

--- mininet-test/mininet-network.py
+++ mininet-test/mininet-network.py
@@ -16,6 +16,8 @@ import socket
 import time
 import os
 import tempfile
+import multiprocessing as mp
+import numpy as np
 
 def runX11WithHost(host, cmd, **kwargs):
     os.environ["MININET_HOST"] = str(host)
@@ -26,6 +28,149 @@ def runX11WithHost(host, cmd, **kwargs):
     for x in kwargs:
         del os.environ[x]
     return ret
+
+class CMultiProc(mp.Process):
+    def wait(self):
+        return self.join()
+    def poll(self):
+#         print("it is", "alive" if self.is_alive() else "dead")
+        return not self.is_alive()
+
+class TraceProcess:
+    def __init__(self):
+        self.mTraceMap = {} # interface: interface object trace, target and few more info
+        self.mRunProc = [] # important
+        self.mRunning = False
+        self.mProcessesToBeTerminated = []
+        self.mProcessesToBeWaited = []
+
+        self.mRunningProc = []
+        self.mProcessesTerminated = []
+
+    def addTrace(self, intf, tracepath, targetIntf): #might add more param
+        assert not self.mRunning
+        traces = np.array([int(float(l.strip().split()[0])) for l in open(tracepath)]) * 8
+        traces = traces.tolist()
+        destIp = targetIntf.IP()
+        name = str(intf)
+        otherInfo = []
+        self.mTraceMap.setdefault(name, []).append([intf, traces, destIp, otherInfo])
+
+    def runX11At(self, at, host, cmd, waitToEnd=False, terminateAfter=0, **env):
+        print("environment:", env)
+        self.mRunProc += [[at, waitToEnd, terminateAfter, "x11", (host, cmd, env)]]
+
+    def runMultiProxAt(self, at, waitToEnd=False, terminateAfter=0, **extra):
+        self.mRunProc += [[at, waitToEnd, terminateAfter, "mp", extra]]
+
+
+    def runIntfCmd(self, intf, cmd):
+        print(cmd)
+        res = intf.cmd(cmd)
+        print(res)
+        return res
+
+    def startTrace(self):
+        for intfName, traceInfos in self.mTraceMap.items():
+            for index, tr in enumerate(traceInfos):
+                intf, trace, destIp, otherInfo = tr
+
+                filterId = index + 3
+                markId = index+10
+                otherInfo.append(filterId)
+                otherInfo.append(markId)
+                otherInfo.append(1)
+
+                self.runIntfCmd(intf, f"tc class add dev {intfName} parent 5:0 classid 5:{filterId} htb rate {trace[0]} ceil {trace[0]} prio 0")
+                self.runIntfCmd(intf, f"iptables -A OUTPUT -t mangle -p tcp -j MARK --set-mark {markId}")
+                self.runIntfCmd(intf, f"tc filter add dev {intfName} parent 5:0 prio 0 protocol ip handle {markId} fw flowid 5:{filterId}")
+                self.runIntfCmd(intf, f"iptables -A OUTPUT -t mangle -p tcp -s {destIp} -j MARK --set-mark {markId}")
+
+    def runTrace(self):
+        for intfName, traceInfos in self.mTraceMap.items():
+            for index, tr in enumerate(traceInfos):
+                intf, trace, destIp, otherInfo = tr
+
+                filterId, markId, nextId = otherInfo
+                otherInfo[2] = (otherInfo[2] + 1) % len(trace)
+
+                self.runIntfCmd(intf, f"tc class change dev {intfName} parent 5:0 classid 5:{filterId} htb rate {trace[nextId]} ceil {trace[nextId]} prio 0")
+
+    def runMpProc(self, args):
+        proc = CMultiProc(**args)
+        proc.start()
+        return (None, proc)
+
+    def runProc(self, nextProc):
+#             at, host, cmd, waitToEnd, terminateAfter, env = nextProc
+            at, waitToEnd, terminateAfter, procType, extra = nextProc
+            proc = None
+            if procType == "x11":
+                host, cmd, env = extra
+                print(f"Runneing `{cmd}`")
+                proc = runX11WithHost(host, cmd, **env)
+            elif procType == "mp":
+                proc = self.runMpProc(extra)
+            assert proc is not None
+            if waitToEnd:
+                self.mProcessesToBeWaited.append(proc)
+            else:
+                self.mProcessesToBeTerminated.append([terminateAfter, proc])
+            self.mRunningProc.append(nextProc)
+
+    def run(self):
+        assert not self.mRunning
+        self.startTrace()
+        self.mRunProc.sort(key=lambda x: x[0])
+        nextTraceAt = 5
+        curTime = 0
+        terminated = False
+        self.mRunning = True
+
+        while True:
+            nextRunAt = nextTraceAt + 2
+            nextTerminateAt = nextTraceAt + 2
+            if len(self.mRunProc):
+                nextRunAt = self.mRunProc[0][0]
+            if terminated and len(self.mProcessesToBeTerminated):
+                nextTerminateAt = self.mProcessesToBeTerminated[0][0]
+
+            if len(self.mRunProc) == 0 and len(self.mProcessesToBeTerminated) == 0 and len(self.mProcessesToBeWaited) == 0:
+                break
+
+            for i, proc in enumerate(self.mProcessesToBeWaited[:]):
+                res = proc[1].poll()
+                if res:
+                    proc = self.mProcessesToBeWaited.pop(i)
+                    self.mProcessesTerminated.append(proc)
+                    if len(self.mProcessesToBeWaited) == 0:
+                        terminated = True
+                        for i, x in enumerate(self.mProcessesToBeTerminated):
+                            x[0] += curTime
+                        self.mProcessesToBeTerminated.sort(key=lambda x: x[0])
+
+            nextActionAt = min(nextTraceAt, nextRunAt, nextRunAt)
+            sleepTime = nextActionAt - curTime
+            assert sleepTime >= 0
+            if sleepTime > 0:
+                time.sleep(sleepTime)
+                curTime += sleepTime
+
+            if nextTraceAt <= curTime:
+                self.runTrace()
+                nextTraceAt += 5
+
+            if nextRunAt <= curTime:
+                assert self.mRunProc[0][0] == nextRunAt
+                nextProc = self.mRunProc.pop(0)
+                self.runProc(nextProc)
+
+            if nextTerminateAt <= curTime:
+                assert self.mProcessesToBeTerminated[0][0] == nextTerminateAt
+                at, proc = self.mProcessesToBeTerminated.pop(0)
+                proc[1].terminate()
+                self.mProcessesTerminated.append(proc)
+
 
 def myNetwork():
 
@@ -73,35 +218,47 @@ def myNetwork():
 
     info( '*** Post configure switches and hosts\n')
 
+#     CLI(net)
+#     net.stop()
+#     return
+
     xterm = "xterm -fa 'Monospace' -fs 12 -bg #460001 -fg #ffffff "
 
-    dpop = None
-    if options.logDir is not None:
-        print("Running pcap")
-        cmd = xterm + "-T '" + h1.name + "'"
-#         cmd += f" dumpcap -i {h1.intf()} -w /tmp/fuck"
-        cmd += f" {WD}/dumpcap.sh"# {h1.intf()} " + os.path.join(options.logDir, "trace.pcap")
-        print(cmd)
-        dpop = runX11WithHost(h1, cmd, MININET_IFC=f"{h1.intf()}", MININET_TRACE_FILE=os.path.join(options.logDir, "trace.pcap"))
-        time.sleep(2)
+#     tp = TraceProcess()
+#     tp.runX11At(3, h2, xterm + "/tmp/ser.sh", terminateAfter=4)
+#     tp.runX11At(5, h1, xterm + "/tmp/cli.sh", terminateAfter=3, IPERF_SERVER_IP = f"{h2.IP()}")
+#     if len(options.traceFiles) > 0:
+#         for i, hn in enumerate([h2] + HN[:]):
+#             tp.addTrace(h1.intfs[0], options.traceFiles[i%len(options.traceFiles)], hn.intfs[0])
+#     intfNames = [x for x in h2.intfList() if str(x).startswith("h2")]
+#     tp.runX11At(6, h2, f"python3.7 /home/exp/dynamic_plot.py {intfNames[0]}", waitToEnd=True)
+#     tp.run()
+#     net.stop()
+#     return
 
-    print("Running server")
+    ##===============
+    tp = TraceProcess()
+    if options.logDir is not None:
+        cmd = xterm + "-T '" + h1.name + "'"
+        cmd += f" {WD}/dumpcap.sh"
+        tp.runX11At(1, h1, cmd, terminateAfter=5, MININET_IFC=f"{h1.intf()}", MININET_TRACE_FILE=os.path.join(options.logDir, "trace.pcap"))
+    if len(options.traceFiles) > 0:
+        for i, hn in enumerate([h2] + HN[:]):
+            tp.addTrace(h1.intfs[0], options.traceFiles[i%len(options.traceFiles)], hn.intfs[0])
+
+    #Adding server
     cmd = xterm + "-T '" + h1.name + "'"
     cmd += f" {WD}/server.sh "
-    setParamFromHost(h1)
-    sterm = runX11WithHost(h1, cmd)
+    henv = setParamFromHost(h1)
+    tp.runX11At(3, h1, cmd, terminateAfter=4, **henv)
 
-    time.sleep(3)
-
-    print("Running client 1")
+    #Adding primary
     cmd = xterm + "-T '" + h2.name + "'"
     cmd += f" {WD}/client-0.sh "
-    setParamFromHost(h2)
-    cterm = runX11WithHost(h2, cmd)
+    henv = setParamFromHost(h2)
+    tp.runX11At(6, h2, cmd, terminateAfter=3, **henv)
 
-    time.sleep(24)
-
-    nterms = []
+    runAfter = 24
     for i in range(options.neighbor):
         print(f"Running client {2+i}")
         hx = HN[i]
@@ -110,27 +267,32 @@ def myNetwork():
             cmd += f" {WD}/client-0.sh "
         else:
             cmd += f" {WD}/client-1.sh "
-        setParamFromHost(hx)
-        oterm = runX11WithHost(hx, cmd)
-        nterms += [oterm]
+        henv = setParamFromHost(hx)
+        tp.runX11At(runAfter + (i*10), hx, cmd, terminateAfter=3, **henv)
 
-        time.sleep(10)
+    tp.runMultiProxAt(4, waitToEnd=True, target=waitForSocket)
 
-
-#     time.sleep(120)
-    print("waiting for socket")
-    waitForSocket()
-    time.sleep(1)
-
-    cterm[1].terminate()
-    for oterm in nterms:
-        oterm[1].terminate()
-    sterm[1].terminate()
-    if dpop is not None:
-        dpop[1].terminate()
-
-    #CLI(net)
+    tp.run()
     net.stop()
+    return
+
+    # End
+
+
+# #     time.sleep(120)
+#     print("waiting for socket")
+#     waitForSocket()
+#     time.sleep(1)
+#
+#     cterm[1].terminate()
+#     for oterm in nterms:
+#         oterm[1].terminate()
+#     sterm[1].terminate()
+#     if dpop is not None:
+#         dpop[1].terminate()
+#
+#     #CLI(net)
+#     net.stop()
 
 def setParamFromHost(node):
     envStr = ""
@@ -140,7 +302,7 @@ def setParamFromHost(node):
     if options.finSock is not None:
         envStr += " -F " + options.finSock
 
-    os.putenv("WEBVIEWTEST_PARAM", envStr)
+    return {"WEBVIEWTEST_PARAM": envStr}
 
 def waitForSocket():
     print("Waiting for socket at", options.finSock)
@@ -167,10 +329,11 @@ def parseCmdArgument():
     parser.add_argument('-m', '--mpd-path', dest='mpdPath', default=os.environ['HOME'] + 'dashed/bbb/media/vid.mpd', type=str)
     parser.add_argument('-L', '--logDir', dest='logDir', default=None, type=str)
     parser.add_argument('-F', '--finishedSocket', dest='finSock', default=tfile, type=str)
+    parser.add_argument('-t', '--trace-file', dest='traceFiles', type=str, nargs='*', default=[])
 
     options = parser.parse_args()
 
-    if not os.path.isdir(options.logDir):
+    if options.logDir is not None and not os.path.isdir(options.logDir):
         os.makedirs(options.logDir)
         os.chmod(options.logDir, 0o777)
 
